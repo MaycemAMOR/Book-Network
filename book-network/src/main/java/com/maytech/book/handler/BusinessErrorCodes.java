607b852e2c5f2800c72acc82747ce66d5e74ebf1
Maycem AMOR <maycemamor@gmail.com>
2024-06-20 21:49:47 +0200

    Exception handling

---
+++ book-network/src/main/java/com/maytech/book/handler/BusinessErrorCodes.java
@@ -0,0 +1,25 @@
+package com.maytech.book.handler;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+import static org.springframework.http.HttpStatus.*;
+
+@Getter
+public enum BusinessErrorCodes {
+    No_code(0, NOT_IMPLEMENTED, "No code"),
+    INCORRECT_CURRENT_PASSWORD(300, BAD_REQUEST, "Current password is incorrect"),
+    NEW_PASSWORD_DOES_NOT_MATCH(301, BAD_REQUEST, "The new password does not match"),
+    ACCOUNT_LOCKED(302, FORBIDDEN, "User account is locked"),
+    ACCOUNT_DISABLED(303, FORBIDDEN, "User account is disabled"),
+    BAD_CREDENTIALS(304, FORBIDDEN, "Login and / or password is incorrect");
+    private final int code;
+    private final String description;
+    private final HttpStatus httpStatus;
+
+    BusinessErrorCodes(int code, HttpStatus httpStatus, String description) {
+        this.code = code;
+        this.description = description;
+        this.httpStatus = httpStatus;
+    }
+}
